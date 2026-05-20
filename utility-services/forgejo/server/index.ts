b5b1f96f7ab489b600730b4ad6f27fa979c9aba0
Shahab Dogar <shahab@dogar.dev>
2026-05-20 18:31:07 +0500

    fix: Forgejo | use env vars for all configs

--- utility-services/forgejo/server/index.ts
+++ utility-services/forgejo/server/index.ts
@@ -1,5 +1,3 @@
-import * as fs from "fs";
-import * as path from "path";
 import { Construct } from "constructs";
 import { KubernetesProvider } from "@cdktf/provider-kubernetes/lib/provider";
 import { ConfigMapV1 } from "@cdktf/provider-kubernetes/lib/config-map-v1";
@@ -32,13 +30,187 @@ export class ForgejoServer extends Construct {
       itemPath: "vaults/Lab/items/forgejo",
     });
 
-    const appIni = fs
-      .readFileSync(path.join(__dirname, "app.ini"), "utf8");
-
-    new ConfigMapV1(this, "config", {
+    new ConfigMapV1(this, "global-config", {
       provider,
-      metadata: { name: `${name}-config`, namespace },
-      data: { "app.ini": appIni },
+      metadata: { name: `${name}-global-config`, namespace },
+      data: {
+        FORGEJO__APP_NAME: "Forgejo: Beyond coding. We forge.",
+        FORGEJO__RUN_MODE: "prod",
+      },
+    });
+
+    new ConfigMapV1(this, "security-config", {
+      provider,
+      metadata: { name: `${name}-security-config`, namespace },
+      data: {
+        FORGEJO__security__INSTALL_LOCK: "true",
+        FORGEJO__security__GLOBAL_TWO_FACTOR_REQUIREMENT: "all",
+      },
+    });
+
+    new ConfigMapV1(this, "cors-config", {
+      provider,
+      metadata: { name: `${name}-cors-config`, namespace },
+      data: {
+        FORGEJO__cors__ENABLED: "true",
+        FORGEJO__cors__ALLOW_DOMAIN: "https://git.dogar.dev",
+      },
+    });
+
+    new ConfigMapV1(this, "repository-config", {
+      provider,
+      metadata: { name: `${name}-repository-config`, namespace },
+      data: {
+        FORGEJO__repository__ENABLE_PUSH_CREATE_USER: "true",
+        FORGEJO__repository__ENABLE_PUSH_CREATE_ORG: "true",
+      },
+    });
+
+    new ConfigMapV1(this, "server-config", {
+      provider,
+      metadata: { name: `${name}-server-config`, namespace },
+      data: {
+        FORGEJO__server__PROTOCOL: "http",
+        FORGEJO__server__ROOT_URL: "https://git.dogar.dev/",
+        FORGEJO__server__SSH_DOMAIN: "git.dogar.dev",
+        FORGEJO__server__DOMAIN: "git.dogar.dev",
+        FORGEJO__server__DISABLE_SSH: "false",
+        FORGEJO__server__SSH_LISTEN_PORT: "2222",
+        FORGEJO__server__SSH_PORT: "2222",
+        FORGEJO__server__SSL_MIN_VERSION: "TLSv1.3",
+        FORGEJO__server__LFS_START_SERVER: "true",
+        FORGEJO__server__ENABLE_PPROF: "false",
+        FORGEJO__server__ENABLE_GZIP: "true",
+        FORGEJO__server__HTTP_PORT: "3000",
+      },
+    });
+
+    new ConfigMapV1(this, "database-config", {
+      provider,
+      metadata: { name: `${name}-database-config`, namespace },
+      data: {
+        FORGEJO__database__DB_TYPE: "postgres",
+        FORGEJO__database__HOST: "postgres-cluster-rw:5432",
+        FORGEJO__database__NAME: "forgejo",
+        FORGEJO__database__USER: "forgejo",
+        FORGEJO__database__SSL_MODE: "verify-full",
+      },
+    });
+
+    new ConfigMapV1(this, "cache-config", {
+      provider,
+      metadata: { name: `${name}-cache-config`, namespace },
+      data: {
+        FORGEJO__cache__ADAPTER: "memory",
+      },
+    });
+
+    new ConfigMapV1(this, "session-config", {
+      provider,
+      metadata: { name: `${name}-session-config`, namespace },
+      data: {
+        FORGEJO__session__PROVIDER: "db",
+      },
+    });
+
+    new ConfigMapV1(this, "queue-config", {
+      provider,
+      metadata: { name: `${name}-queue-config`, namespace },
+      data: {
+        FORGEJO__queue__TYPE: "channel",
+      },
+    });
+
+    new ConfigMapV1(this, "storage-config", {
+      provider,
+      metadata: { name: `${name}-storage-config`, namespace },
+      data: {
+        FORGEJO__storage__STORAGE_TYPE: "minio",
+        FORGEJO__storage__MINIO_USE_SSL: "true",
+        FORGEJO__storage__MINIO_BUCKET: "forgejo",
+        FORGEJO__storage__MINIO_ENDPOINT: "blob.dogar.dev:443",
+      },
+    });
+
+    new ConfigMapV1(this, "service-config", {
+      provider,
+      metadata: { name: `${name}-service-config`, namespace },
+      data: {
+        FORGEJO__service__DISABLE_REGISTRATION: "true",
+      },
+    });
+
+    new ConfigMapV1(this, "indexer-config", {
+      provider,
+      metadata: { name: `${name}-indexer-config`, namespace },
+      data: {
+        FORGEJO__indexer__ISSUE_INDEXER_TYPE: "db",
+      },
+    });
+
+    new ConfigMapV1(this, "oauth2-client-config", {
+      provider,
+      metadata: { name: `${name}-oauth2-client-config`, namespace },
+      data: {
+        FORGEJO__oauth2_client__ENABLE_AUTO_REGISTRATION: "true",
+        FORGEJO__oauth2_client__UPDATE_AVATAR: "true",
+      },
+    });
+
+    new ConfigMapV1(this, "openid-config", {
+      provider,
+      metadata: { name: `${name}-openid-config`, namespace },
+      data: {
+        FORGEJO__openid__ENABLE_OPENID_SIGNIN: "false",
+      },
+    });
+
+    new ConfigMapV1(this, "mailer-config", {
+      provider,
+      metadata: { name: `${name}-mailer-config`, namespace },
+      data: {
+        FORGEJO__mailer__ENABLED: "true",
+        FORGEJO__mailer__PROTOCOL: "smtp+starttls",
+        FORGEJO__mailer__SMTP_ADDR: "smtp.protonmail.ch",
+        FORGEJO__mailer__SMTP_PORT: "587",
+        FORGEJO__mailer__FROM: "git@dogar.dev",
+        FORGEJO__mailer__USER: "git@dogar.dev",
+      },
+    });
+
+    new ConfigMapV1(this, "picture-config", {
+      provider,
+      metadata: { name: `${name}-picture-config`, namespace },
+      data: {
+        FORGEJO__picture__GRAVATAR_SOURCE: "gravatar",
+        FORGEJO__picture__ENABLE_FEDERATED_AVATAR: "true",
+      },
+    });
+
+    new ConfigMapV1(this, "metrics-config", {
+      provider,
+      metadata: { name: `${name}-metrics-config`, namespace },
+      data: {
+        FORGEJO__metrics__ENABLED: "true",
+      },
+    });
+
+    new ConfigMapV1(this, "actions-config", {
+      provider,
+      metadata: { name: `${name}-actions-config`, namespace },
+      data: {
+        FORGEJO__actions__ENABLED: "true",
+        FORGEJO__actions__DEFAULT_ACTIONS_URL: "https://code.forgejo.org",
+      },
+    });
+
+    new ConfigMapV1(this, "log-config", {
+      provider,
+      metadata: { name: `${name}-log-config`, namespace },
+      data: {
+        FORGEJO__log__LEVEL: "info",
+        FORGEJO__log__MODE: "console",
+      },
     });
 
     new ConfigMapV1(this, "pg-config", {
@@ -100,64 +272,32 @@ export class ForgejoServer extends Construct {
           spec: {
             nodeSelector: { nodepool: "worker" },
             securityContext: { fsGroup: "1000" },
-            initContainer: [
-              {
-                name: "forgejo-setup",
-                image: "codeberg.org/forgejo/forgejo:15-rootless",
-                envFrom: [
-                  { configMapRef: { name: `${name}-pg-config` } },
-                  { secretRef: { name: 'forgejo' } },
-                ],
-                volumeMount: [
-                  {
-                    name: "data",
-                    mountPath: "/var/lib/gitea",
-                  },
-                  {
-                    name: "config",
-                    mountPath: "/etc/gitea/app.ini",
-                    subPath: "app.ini",
-                  },
-                  {
-                    name: "ssl-bundle",
-                    mountPath: "/opt/forgejo/.postgresql",
-                    readOnly: true,
-                  },
-                ],
-                command: [
-                  "/bin/sh",
-                  "-c",
-                  [
-                    'mkdir -p /var/lib/gitea/custom/conf',
-                    '&&',
-                    'test -f /var/lib/gitea/custom/conf/app.ini || cp /etc/gitea/app.ini /var/lib/gitea/custom/conf/app.ini',
-                    '&&',
-                    'forgejo admin user create --admin',
-                    '--username "$FORGEJO_ADMIN_USER"',
-                    '--password "$FORGEJO_ADMIN_PASSWORD"',
-                    '--email "$FORGEJO_ADMIN_EMAIL"',
-                    '--config /var/lib/gitea/custom/conf/app.ini',
-                    "2>/dev/null || true",
-                    '&&',
-                    'forgejo admin auth add-oauth',
-                    '--name authentik',
-                    '--provider openidConnect',
-                    '--key "$FORGEJO_OAUTH_KEY"',
-                    '--secret "$FORGEJO_OAUTH_SECRET"',
-                    '--auto-discover-url',
-                    '"https://auth.dogar.dev/application/o/forgejo/.well-known/openid-configuration"',
-                    '--config /var/lib/gitea/custom/conf/app.ini',
-                    "2>/dev/null || true",
-                  ].join(" "),
-                ],
-              },
-            ],
             container: [
               {
                 name: "forgejo",
                 image: "codeberg.org/forgejo/forgejo:15-rootless",
                 envFrom: [
+                  { configMapRef: { name: `${name}-global-config` } },
+                  { configMapRef: { name: `${name}-security-config` } },
+                  { configMapRef: { name: `${name}-cors-config` } },
+                  { configMapRef: { name: `${name}-repository-config` } },
+                  { configMapRef: { name: `${name}-server-config` } },
+                  { configMapRef: { name: `${name}-database-config` } },
+                  { configMapRef: { name: `${name}-cache-config` } },
+                  { configMapRef: { name: `${name}-session-config` } },
+                  { configMapRef: { name: `${name}-queue-config` } },
+                  { configMapRef: { name: `${name}-storage-config` } },
+                  { configMapRef: { name: `${name}-service-config` } },
+                  { configMapRef: { name: `${name}-indexer-config` } },
+                  { configMapRef: { name: `${name}-oauth2-client-config` } },
+                  { configMapRef: { name: `${name}-openid-config` } },
+                  { configMapRef: { name: `${name}-mailer-config` } },
+                  { configMapRef: { name: `${name}-picture-config` } },
+                  { configMapRef: { name: `${name}-metrics-config` } },
+                  { configMapRef: { name: `${name}-actions-config` } },
+                  { configMapRef: { name: `${name}-log-config` } },
                   { configMapRef: { name: `${name}-pg-config` } },
+                  { secretRef: { name: "forgejo" } },
                 ],
                 port: [
                   { name: "http", containerPort: 3000 },
@@ -167,11 +307,6 @@ export class ForgejoServer extends Construct {
                   {
                     name: "data",
                     mountPath: "/var/lib/gitea",
-                  },
-                  {
-                    name: "config",
-                    mountPath: "/etc/gitea/app.ini",
-                    subPath: "app.ini",
                   },
                   {
                     name: "ssl-bundle",
@@ -221,10 +356,6 @@ export class ForgejoServer extends Construct {
               {
                 name: "data",
                 persistentVolumeClaim: { claimName: `${name}-data` },
-              },
-              {
-                name: "config",
-                configMap: { name: `${name}-config` },
               },
               {
                 name: "ssl-bundle",
